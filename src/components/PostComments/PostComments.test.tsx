f6dea04fab0e38d437c9b90ab9bc7919ed344fde
redssy <redlemagic@hotmail.com>
2024-10-31 18:32:23 -0300

    fix: subindo testes insercao comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,52 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
+const testarAdcComentario = (executarDebug: Function, comentario: string) => {
+    // escreve texto no 'textArea'
+    fireEvent.change(screen.getByTestId('id-texto'), {
+        target: {
+            value: comentario
+        }
+    })
+    // aciona botao insercao
+    fireEvent.click(screen.getByTestId('id-btncomentar'));
+    // efetua debug do test
+    executarDebug();
+    // oque se espera: expectativa
+    expect(screen.getByText(comentario)).toBeInTheDocument()
+}
+
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    it('Deve renderizar o componente corretamente', () => { 
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+});
+
+    // cria uma 'suite' p/ os testes de adicao de comentarios
+describe('Teste p/ adicionar dois comentarios', () => {
+    // teste 1
+    test('Teste adicionando comentario 1', () => {
+        // renderiza componente
+        const { debug }= render(<PostComment/>);
+        // testa comentario
+        testarAdcComentario(debug, 'ola');
+    });
+    // teste 2
+    test('Teste adicionando comentario 2', () => {
+        // renderiza componente
+        const { debug }= render(<PostComment/>);
+        // testa comentario
+        testarAdcComentario(debug, 'fulano');
+    });
+    // teste 3
+    test('Teste adicionando 2 comentarios', () => {
+        // renderiza componente
+        const { debug }= render(<PostComment/>);
+        // testa comentario
+        testarAdcComentario(debug, 'agora');
+        testarAdcComentario(debug, 'eh dois');
+        // expectativa
+        expect(screen.getAllByTestId('id-itens')).toHaveLength(2)
     });
 });
